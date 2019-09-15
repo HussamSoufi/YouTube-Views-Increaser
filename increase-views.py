c7008a52d87d1f3aa6f0ce9fb3ac1076279a3f5c
Hussam Soufi <hussam1soufi@gmail.com>
2019-09-15 13:48:56 +0200

    initial

---
+++ increase-views.py
@@ -0,0 +1,10 @@
+#@Hussam Soufi
+import webbrowser
+import time
+import os
+url = "https://www.youtube.com/watch?v=G4gWUnzRLQU&t=4s"
+while True:
+    webbrowser.open(url)
+    time.sleep(1.5*60) #set the time of the video
+    #os.system("taskkill /im firefox.exe /f")
+    os.system("taskkill /im chrome.exe /f")
