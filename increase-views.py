50f0660bce3aebf8a7c91a280946440a5d2785a0
Hussam Soufi <hussam1soufi@gmail.com>
2019-10-18 17:38:22 +0200

    Update increase-views.py

--- increase-views.py
+++ increase-views.py
@@ -3,9 +3,9 @@
 import webbrowser
 import time
 import os
-url = "https://www.youtube.com/watch?v=G4gWUnzRLQU&t=4s"
+url = "https://www.youtube.com/watch?v=G4gWUnzRLQU&t=4s" #the video URL
 while True:
     webbrowser.open(url)
-    time.sleep(1.5*60) #set the time of the video
-    #os.system("taskkill /im firefox.exe /f")
-    os.system("taskkill /im chrome.exe /f")
+    time.sleep(1.5*60) #set the time of the video in seconds
+    #os.system("taskkill /im firefox.exe /f") #if you use firefox
+    os.system("taskkill /im chrome.exe /f")   #if you use chrome
